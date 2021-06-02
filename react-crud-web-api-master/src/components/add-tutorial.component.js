52f90d5768ad946850d0a1363146e6e69be7c75a
mylinhpham <myylinhpham@gmail.com>
2021-06-02 15:44:30 -0700

    made css changes

---
+++ react-crud-web-api-master/src/components/add-tutorial.component.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+
+export default class AddTutorial extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.saveTutorial = this.saveTutorial.bind(this);
+    this.newTutorial = this.newTutorial.bind(this);
+    this.onChangeBrand = this.onChangeBrand.bind(this);
+
+    this.state = {
+      id: null,
+      title: "",
+      description: "",
+      brand: "",
+      published: false,
+
+      submitted: false
+    };
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeBrand(e) {
+    this.setState({
+      brand: e.target.value
+    });
+  }
+
+  saveTutorial() {
+    var data = {
+      title: this.state.title,
+      description: this.state.description,
+      brand: this.state.brand
+    };
+
+    TutorialDataService.create(data)
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          title: response.data.title,
+          description: response.data.description,
+          brand: response.data.brand,
+          published: response.data.published,
+
+          submitted: true
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  newTutorial() {
+    this.setState({
+      id: null,
+      title: "",
+      description: "",
+      brand: "",
+      published: false,
+
+      submitted: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={this.newTutorial}>
+              Add New Shoe
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                required
+                value={this.state.title}
+                onChange={this.onChangeTitle}
+                name="title"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                name="description"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="brand">Brand</label>
+              <input
+                  type="text"
+                  className="form-control"
+                  id="brand"
+                  required
+                  value={this.state.brand}
+                  onChange={this.onChangeBrand}
+                  name="brand"
+              />
+            </div>
+
+            <button onClick={this.saveTutorial} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
